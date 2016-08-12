7761e0a6cd3f3de86a667baf01c683d47538bc1f
TJ2001 <jung.m.tim@gmail.com>
2016-08-12 10:37:03 -0700

    add style to selected and add new-food.component.ts that makes form to add food items

--- app/food-list.component.ts
+++ app/food-list.component.ts
@@ -1,19 +1,20 @@
 import { Component, EventEmitter } from 'angular2/core';
 import { FoodComponent } from './food.component';
 import { Food } from './food.model';
+import { NewFoodComponent } from './new-food.component'
 
 @Component({
   selector: 'food-list',
   inputs: ['foodList'],
   outputs: ['onFoodSelect'],
-  directives: [FoodComponent],
+  directives: [FoodComponent, NewFoodComponent],
   template: `
   <food-display *ngFor="#currentFood of foodList"
     (click)="foodClicked(currentFood)"
     [class.selected]="currentFood === selectedFood"
     [food]="currentFood">
   </food-display>
-
+  <new-food (onNewFood)="addFood($event)"></new-food>
   `
 })
 export class FoodListComponent {
@@ -28,11 +29,8 @@ export class FoodListComponent {
     this.selectedFood = clickedFood;
     this.onFoodSelect.emit(clickedFood);
   }
+  addFood(Food) {
+    console.log("addFood Called");
+    this.foodList.push(Food);
+  }
 }
-
-
-
-// <food-display *ngFor="#currentFood of foodList"
-//   (click)="foodClicked(currentFood)"
-//   [class.selected]="currentFood === selectedFood"
-//   [food]="currentFood">
